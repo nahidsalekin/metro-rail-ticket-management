08f28cc4e1c74af6f620f6ac9e834bcaf50343e3
Nahid <khannahid64@gmail.com>
2020-04-14 00:48:14 +0600

    :fire: reservation data extracted

--- main/views.py
+++ main/views.py
@@ -21,21 +21,67 @@ class Login(TemplateView):
 
 class About(TemplateView):
     template_name="about.html"
-    
+from datetime import datetime
+import time
+def profile(request):
+    u = user.objects.get(username=request.session['username'])
+    total = booking.objects.filter(number=u.number).count()
+    if total != 0:    
+        last = booking.objects.filter(number=u.number).latest('date')
+        last = last.date
+        today = datetime.now()    
+        active = booking.objects.filter(Q(number=u.number,date__gt = today) | Q(number=u.number,date=today))
+    else:
+        last = "..." 
+        active = ""   
+    return render(request,"profile.html",{'fullname': u.fullname,'username': u.username,'email': u.email,'gender': u.gender,'age': u.age,'number': u.number,'password': u.password,'total': total,'last': last,'active': active})
+
+def users(request):
+    u = user.objects.all()
+    return render(request,"users.html",{'all_user':u})
+
+
+def update_profile(request):
+    fullname = request.POST["fullname"]
+    username = request.POST["username"]
+    email = request.POST["email"]
+    age = request.POST["age"]
+    gender = request.POST["gender"]
+    number = request.POST["number"]
+    password = request.POST["password"]
+    u = user.objects.get(username=username)
+    u.fullname = fullname
+    u.email = email
+    u.gender = gender
+    u.number = number
+    u.age = age
+    u.password = password
+    u.save()
+    total = booking.objects.filter(number=u.number).count()    
+    if total != 0:    
+        last = booking.objects.filter(number=u.number).latest('date')
+        last = last.date
+        today = datetime.now()  
+        active = booking.objects.filter(Q(number=u.number,date__gt = today) | Q(number=u.number,date=today))
+    else:
+        last = "..."  
+    messages.add_message(request, messages.INFO, 'Update Complete.')
+    return render(request,"profile.html",{'fullname': u.fullname,'username': u.username,'email': u.email,'gender': u.gender,'age': u.age,'number': u.number,'password': u.password,'total': total,'last': last,'active': active})
+
+def booking_list(request):
+    all_booking = booking.objects.all()
+    all_user = user.objects.all()
+    return render(request,"booking_list.html",{'all_booking': all_booking,'all_user': all_user})    
+
 
 def check_ticket(request):
     info = ticket.objects.all()
     slots = 0
     for init in info:
         slots = slots+init.slots
-    total = 0    
-    ticket_info = booking.objects.all()
-    for passenger in ticket_info:
-        total = total + passenger.tickets
-    tickets = (slots - total)/2
     slots = int(slots/2)    
 
-    return render(request,"ticket.html",{'tickets': range(slots)})
+    return render(request,"ticket.html",{'tickets': range(slots),'capacity': slots*2})
  
 def Location(request):
     all_info = location.objects.all()    
@@ -64,24 +110,27 @@ def logout(request):
 def user_login(request):
     username = request.POST["username"]
     password = request.POST["password"]
-    u = user.objects.get(username=username)
- 
-    if u.password == password:
-        request.session['username'] = u.username
-        return render(request,"index.html")
-    else:
-        return render(request,"login.html")   
+    count = user.objects.filter(username=username).count()
+    if count!=0:
+        u = user.objects.get(username=username)
+        if u.password == password:
+            request.session['username'] = u.username
+            return render(request,"index.html")
+    
+    messages.add_message(request, messages.INFO, 'Wrong User') 
+    return render(request,"login.html")   
 
 def submit_user(request):
     fullname = request.POST["fullname"]
-    username = request.POST["fullname"]
+    username = request.POST["username"]
     email = request.POST["email"]
     gender = request.POST["gender"]
+    age = request.POST["age"]
     number = request.POST["number"]
     password = request.POST["password"]
     count = user.objects.filter(Q(username=username) | Q(email=email) | Q(number=number)).count()
     if count == 0:
-        main_info = user(fullname=fullname,username=username,email=email,gender=gender,number=number,password=password)
+        main_info = user(fullname=fullname,username=username,email=email,gender=gender,age=age,number=number,password=password)
         main_info.save()
         messages.add_message(request, messages.INFO, 'Registration Successful.')
     else:
@@ -167,6 +216,8 @@ def ticket_availability(request):
             for passenger in ticket_info:
                 total = total + passenger.tickets
             tickets = info.slots - total
+            if tickets <= 0:
+                tickets = 0
         data = json.dumps({'distance': distance,'price': price,'tickets': tickets})
         return JsonResponse(data,content_type='application/json',safe=False)           
     return HttpResponse("0")
@@ -190,15 +241,11 @@ def calculate_distance(lat1,lon1,lat2,lon2):
         
 import re
 def confirm_booking(request):
-    if request.session._session:
-        print("login firsst")
-        return render(request,"ticket.html")
-        return HttpResponseRedirect('login')
-    elif request.method == 'POST':
+    if request.method == 'POST':
         start_point = request.POST["start_point"]
         destination = request.POST["destination"]
         date = request.POST["date"]
-        class_type = "student"
+        class_type = request.POST["class_type"]
         time = request.POST["time"]
         quantity = int(request.POST["quantity"])
         seat_index = request.POST["seat_index"]
@@ -228,24 +275,52 @@ def purchase(request):
         class_type = request.POST["class_type"]
         time = request.POST["time"]
         date = request.POST["date"]
-        if class_type == "Custom Choice":
-            seat_no = request.POST["seat_no"]
-            seat_no = seat_no[:-1]
-            seat_index = request.POST["seat_index"]
-            quantity = request.POST["quantity"]
-            seats = quantity + " (" + seat_no + ")"
-            quantity = int(quantity)
-            class_type = "student"
-        else:
-            quantity = request.POST["tickets"]
-            seats = quantity
-            quantity = int(quantity)
+        seat_no = request.POST["seat_no"]
+        seat_no = seat_no[:-1]
+        seat_index = request.POST["seat_index"]
+        quantity = request.POST["tickets"]
+        seats = quantity + " (" + seat_no + ")"
+        quantity = int(quantity)
+    
         info = ticket.objects.get(class_type=class_type)
         distance, base_fare = calculate_price(start_point,destination,info.price)
         price = quantity*base_fare
-        return render(request,"purchase.html",{'from':start_point,'to':destination,'distance':distance,'date':date,'time':time,'seats':seats,'quantity':quantity,'price':price,'seat_index':seat_index}) 
+        return render(request,"purchase.html",{'from':start_point,'to':destination,'distance':distance,'date':date,'time':time,'seats':seats,'quantity':quantity,'price':price,'seat_index':seat_index,'class_type':class_type}) 
     return render(request,"purchase.html")
 
+def cancel_reservation(request):
+    if request.method == 'POST':
+        start_point = request.POST["start_point"]
+        destination = request.POST["destination"]
+        date = request.POST["date"]
+        time = request.POST["time"]
+        u = user.objects.get(username=request.session['username'])
+        booking.objects.filter(start_point=start_point,destination=destination,date=date,time=time,number=u.number).delete()
+        total = booking.objects.filter(number=u.number).count()
+        if total != 0:    
+            last = booking.objects.filter(number=u.number).latest('date')
+            last = last.date
+            today = datetime.now()    
+            active = booking.objects.filter(Q(number=u.number,date__gt = today) | Q(number=u.number,date=today))
+        else:
+            last = "..."    
+        messages.add_message(request, messages.INFO, 'Reservation Cancelled.')    
+        return render(request,"profile.html",{'fullname': u.fullname,'username': u.username,'email': u.email,'gender': u.gender,'age': u.age,'number': u.number,'password': u.password,'total': total,'last': last,'active': active})
+    return render(request,"index.html")
+
+def cancel_reservation_admin(request):
+    if request.method == 'POST':
+        start_point = request.POST["start_point"]
+        destination = request.POST["destination"]
+        date = request.POST["date"]
+        time = request.POST["time"]
+        number = request.POST["number"]
+        print(number)
+        booking.objects.filter(start_point=start_point,destination=destination,date=date,time=time,number=number).delete()
+        all_booking = booking.objects.all()
+        all_user = user.objects.all()
+        return render(request,"booking_list.html",{'all_booking': all_booking,'all_user': all_user})    
+    return render(request,"index.html")
 
 def occupied_seat(request):
     if request.method == 'POST':    
